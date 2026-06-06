8d1a1dc60c9aa95119a1812e0255e2c893d903b0
sha2cs <sha2cs.hack@gmail.com>
2026-06-05 20:53:16 -0500

    Ejercicio de IMC

---
+++ src/app/Principal.java
@@ -0,0 +1,15 @@
+package app;
+
+import javax.swing.SwingUtilities;
+
+public class Principal {
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            VentanaPrincipal ventana = new VentanaPrincipal();
+            ventana.setVisible(true);
+        });
+    }
+    
+}
